67dbe6954d6f7c67b53543e8b9d23267e9643b2a
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-08-27 05:17:34 -0400

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 let currentUser = 'Jean';
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+// First way to do it
 let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+// Second way to do it
+// let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
